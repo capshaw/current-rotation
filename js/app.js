46cdc92a40ffa9771e868155ec5c2350a4723216
Andrew Capshaw <andrew.capshaw@gmail.com>
2014-03-02 17:15:49 -0600

    Initial app commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/**
+ * Gets my Rdio 'Current Rotation' playlist via my proxy at api.capshaw.me. The
+ * proxy server handles all of the hard parts and keeps my API key secret.
+ *
+ * As a note: usually, you would never build an Angular.js app in one file.
+ * However, the scope of this project is so small that I figured it would be
+ * silly to split it into multiple files.
+ */
+'use strict';
+
+var api_url = 'http://api.capshaw.me/rdio/rotation/';
+var rotation = angular.module('rotation', []);
+
+rotation.controller('RotationCtrl', function RotationCtrl($scope, $http) {
+    $http({
+        method: 'GET',
+        url: api_url
+    }).
+    success(function(data, status, headers, config) {
+        $scope.playlist = data;
+        setTimeout(function(){
+            $scope.show = true;
+        }, 1000);
+    }).
+    error(function(data, status, headers, config) {
+        $scope.error = true;
+        $scope.error_message = status;
+    });
+});
